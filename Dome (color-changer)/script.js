5202cc861d35cf9b8c3f4abf08d338345e9a7730
waleed1857 <163301387+waleed1857@users.noreply.github.com>
2024-03-20 10:38:02 +0500

    Update script.js

--- Dome (color-changer)/script.js
+++ Dome (color-changer)/script.js
@@ -4,6 +4,7 @@ var redBtn = document.getElementById("redBtn");
 var grnBtn = document.getElementById("grnBtn");
 var bluBtn = document.getElementById("bluBtn");
 var blackBtn = document.getElementById("blackBtn");
+var hexCode = document.getElementById("hex-code");
 // var myBody = document.body;
 
 
@@ -18,18 +19,22 @@ blackBtn.style.background = "black";
 // Event listner to perform operation
 whiteBtn.addEventListener("click", function(){
     document.body.style.background = "white";  // to change background color
+    document.getElementById("hex-code").innerHTML = "Hex Code:  #FFFFFF";
 });
 redBtn.addEventListener("click", function(){
     document.body.style.background = "red";  // to change background color
+    document.getElementById("hex-code").innerHTML = "Hex Code:  #FF0000";
 });
 grnBtn.addEventListener("click", function(){
     document.body.style.background = "green";  // to change background color
+    document.getElementById("hex-code").innerHTML = "Hex Code:  #00FF00";
 });
 bluBtn.addEventListener("click", function(){
     document.body.style.background = "blue";  // to change background color
+    document.getElementById("hex-code").innerHTML = "Hex Code:  #0000FF";
 });
 blackBtn.addEventListener("click", function(){
     document.body.style.background = "black";  // to change background color
+    document.getElementById("hex-code").innerHTML = "Hex Code:  #000000";
 });
-
 
